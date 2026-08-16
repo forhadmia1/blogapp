a2df19203b4cf886a8a5935f01b20ba5251e5926
forhadmia1 <forhad8613@gmail.com>
2026-08-17 00:56:08 +0600

    feat: add admin user seeding script and corresponding npm command

---
+++ src/scripts/seedAdmin.ts
@@ -0,0 +1,60 @@
+import "dotenv/config";
+import { prisma } from "../lib/prisma";
+import { USER_ROLE } from "../middleware/auth";
+
+async function seedAdminUser() {
+    try {
+
+        const payload = {
+            name: 'Admin',
+            email: process.env.ADMIN_EMAIL as string,
+            password: process.env.ADMIN_PASSWORD as string,
+            role: USER_ROLE.ADMIN,
+            status: 'ACTIVE',
+        }
+
+        console.log('***checking admin user***')
+
+        const existUser = await prisma.user.findUnique({
+            where: { email: payload.email }
+        })
+
+        console.log('***exist user***', existUser)
+
+        if (existUser) {
+            throw Error('Admin user already exists')
+        }
+
+        console.log('***creating admin user***')
+        const signupAdmin = await fetch(`${process.env.APP_URL}/api/auth/sign-up/email`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Origin': process.env.APP_URL as string
+            },
+            body: JSON.stringify(payload)
+        })
+
+
+        const data = await signupAdmin.json();
+
+        console.log('***updating user data***', data)
+        if (data) {
+            await prisma.user.update({
+                where: { email: payload.email },
+                data: {
+                    emailVerified: true
+                }
+            })
+        }
+
+        console.log("***admin user created successfully***", data)
+        return data
+    } catch (error) {
+        console.error("Error creating admin user", error)
+        return null
+    }
+}
+
+seedAdminUser()
+
